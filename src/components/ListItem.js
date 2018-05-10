6d4af04ff8c2f2db9e6c6d4dc6d4207f6906ba1d
Natasha Koh <NatashaKSS@users.noreply.github.com>
2018-05-10 16:21:18 +0800

    Added listitem's selected library action creator

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
 
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -23,4 +32,7 @@ const styles = {
   },
 };
 
-export default ListItem;
+// Don't need mapStateToProps, but want to bind our action creators
+// to this component. So this is an equivalent to a map actions to
+// props
+export default connect(null, actions)(ListItem);
